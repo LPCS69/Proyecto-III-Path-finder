963c767149903605dad1b59c32f3d81701e2a8cc
LPCS <l_pablocs@hotmail.com>
2021-11-19 15:39:05 -0600

    Inicio de Interfaz

--- TecGraph.py
+++ TecGraph.py
@@ -2,6 +2,8 @@
 """
 Simple Program to help you get started with Google's APIs
 """
+from tkinter import *
+from tkinter import ttk
 import urllib.request, json
 def distancias(ori, des):
     origin = ori.replace(' ','+')
@@ -18,7 +20,31 @@ def distancias(ori, des):
     directions = json.loads(response)
     routes = directions['routes']
     metros = routes[0]['legs']
+    print(metros[0]['distance']['value'])
     return metros[0]['distance']['value']
-
-
 distancias("Zona Deportiva, Provincia de Cartago, Cartago","Canchas Sinteticas ITCR, Provincia de Cartago, Cartago")
+ventana = Tk()
+ventana.title("TecGraph") 
+ventana.minsize(900,600) 
+ventana.maxsize(900,600)
+canvas = Canvas(ventana , width= 330, height = 310, bg = "#50328B")
+imagen = PhotoImage(file = "tecgraph.png")
+canvas.create_image(20,10,anchor=NW,image = imagen)
+canvas.place(x=515, y=15)
+Distancia=Button(ventana, text="Distancia",width=15, height=3, command=distancias("Zona Deportiva, Provincia de Cartago, Cartago"
+                                                                      ,"Canchas Sinteticas ITCR, Provincia de Cartago, Cartago"))
+com_origen = ttk.Combobox(ventana)
+com_origen.place(x=125, y=50)
+com_origen.configure(width=30, height=50)
+com_origen["values"] = [" Zona Deportiva"]
+combo_desti = ttk.Combobox(ventana)
+combo_desti.configure(width=30, height=50)
+combo_desti.place(x=125, y=150)
+combo_desti["values"] = [" Canchas Sinteticas ITCR"]
+Distancia.place(x=20,y=200)
+Distancia.configure(width=10, height=2)
+Origen = Label(ventana, text="Origen")
+Origen.place(x=20, y=50)
+Destino = Label(ventana, text="Destino")
+Destino.place(x=20, y=150)
+ventana.mainloop()
